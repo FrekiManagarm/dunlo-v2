53802cad1d32b2689b9d277afd4971652d6c90ee
Mathieu Chambaud <mathchambaud2000@gmail.com>
2026-05-23 16:32:38 +0200

    feat(marketing): use fumadocs for blog mdx

---
+++ apps/marketing/source.config.ts
@@ -0,0 +1,17 @@
+import { pageSchema } from "fumadocs-core/source/schema";
+import { defineCollections } from "fumadocs-mdx/config";
+import { z } from "zod";
+
+export const blogPosts = defineCollections({
+  type: "doc",
+  dir: "content/blog",
+  schema: pageSchema.extend({
+    description: z.string(),
+    date: z.string(),
+    published: z.boolean().default(true),
+    tags: z.array(z.string()).default([]),
+    keywords: z.array(z.string()).default([]),
+    author: z.string().optional(),
+    readingTime: z.string().optional(),
+  }),
+});
